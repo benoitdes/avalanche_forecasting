02b7dc6b8471120f1af4557154cd290bcedf6c94
plume <plume@plumes-MacBook-Air.local>
2019-06-14 19:52:46 +0200

    Split module into different ones

--- tools_function.py
+++ tools_function.py
@@ -18,8 +18,25 @@ import matplotlib.cm as cm
 
 idx = pd.IndexSlice
 
+def parse_html(url):
+
+    html = urlopen(url)
+    soup = BeautifulSoup(html)
+
+    return soup
 
 
+def get_avalanche_info(soup_object):
+
+    q = soup.find_all('script')[1]
+    a = q.get_text()
+    b = a.split('var avalanche = ')[1]
+    c = b[len('JSON.parse(\\'):][:-6]
+    c = c.replace(r'\"', '"').replace(r'\"', '"')
+    info = json.loads(c)
+    return {k: [info[k]] for k in keys}
+
+    
 
 #### populate a pandas dataframe with data from data-avalanche.org ####
 
@@ -36,19 +53,9 @@ links = driver.find_elements_by_partial_link_text('avalanche')
 for link in links[:20]:
     time.sleep(2)
     url = link.get_attribute('href')
-    html = urlopen(url)
-    soup = BeautifulSoup(html)
-    q = soup.find_all('script')
-    q = q[1]
-    a = q.get_text()
-    b = a.split('var avalanche = ')[1]
-    c = b[len('JSON.parse(\\'):]
-    c = c[:-6]
-    c = c.replace(r'\"', '"').replace(r'\"', '"')    
-    final = json.loads(c)
-    new_dict = {k: [final[k]] for k in keys}
-    print(new_dict)
-    data = pd.DataFrame(new_dict)
+    soup = parse_html(url)
+    dic_data = get_avalanche_info(soup)
+    data = pd.DataFrame(dic_data)
     final_data = final_data.append(data)
 
 
@@ -96,110 +103,29 @@ keys = ['id',
 
 
 
+
+
+
 ### downoad BRA 
 
-url = 'https://donneespubliques.meteofrance.fr/?fond=produit&id_produit=265&id_rubrique=50'
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from urllib.request import urlopen, urlretrieve
-from bs4 import BeautifulSoup
-import time
-import json
-from collections import defaultdict
+
+
+
+    
+import glob
+report_downloaded(zone, date)  
+zone = 'CHABLAIS'
+date = '04142016'
+print(f'../avalanche_forecasting/avalanche_report/{zone}_{date}.xml')
+    
+
+
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.get(url)
 
-
-################################################
-#### find available date ###########
-################################################
-################################################
-
-driver.find_element_by_xpath("//div[@class='publication-info telechargements replie']/h3[contains(., 'Téléchargement')]").click()
-driver.find_element_by_id('datepicker').click()
-
-json_date = defaultdict(lambda : defaultdict(dict))
-
-dropdown_year = Select(driver.find_element_by_xpath("//select[@class='ui-datepicker-year']"))
-available_year = [year_option.text for year_option in dropdown_year.options]
-for year in available_year[:2]:
-    driver.find_element_by_xpath(f"//select[@class='ui-datepicker-year']/option[text()='{year}']").click()
-    dropdown_month = Select(driver.find_element_by_xpath("//select[@class='ui-datepicker-month']"))
-    available_month = [month_option.text for month_option in dropdown_month.options] #month_option.get_attribute('value')
-    print(available_month)
-    for month in available_month:
-        driver.find_element_by_xpath(f"//select[@class='ui-datepicker-month']/option[text()='{month}']").click()
-        available_days = [day.text for day in driver.find_elements_by_xpath("//td[@data-handler='selectDay']")]
-        json_date[year][month] = available_days
-
-#########################################
-#########################################
-#########################################
-
-
-
-################################################
-#### download BRA from json_date (webdriver need to be open on the 'normal page') ###########
-################################################
-################################################
-        
-        
-month_equivalent = {'janv.': '01',
-                    'févr.': '02',
-                    'mars': '03',
-                    'avril': '04',
-                    'mai': '05',
-                    'juin': '06',
-                    'juil.': '07',
-                    'août': '08',
-                    'sept.': '09', 
-                    'oct.': '10',
-                    'nov.': '11',
-                    'déc.': '12'}        
-
-        
-driver.find_element_by_xpath("//div[@class='publication-info telechargements replie']/h3[contains(., 'Téléchargement')]").click()
-driver.find_element_by_id('datepicker').click()
-for year, available_month in json_date.items():
-    print(year)
-    driver.find_element_by_xpath(f"//select[@class='ui-datepicker-year']/option[text()='{year}']").click()
-    for month, available_days in available_month.items():
-        print(month)
-        driver.find_element_by_xpath(f"//select[@class='ui-datepicker-month']/option[text()='{month}']").click()
-        for day in available_days:
-            date = f"{month_equivalent[month]}{day}{year}"
-            print(day)
-            driver.find_element_by_xpath(f"//td[@data-handler='selectDay']/a[text()='{day}']").click()
-            dropdown_massif = Select(driver.find_element_by_xpath("//select[@id='select_massif']"))
-            if not dropdown_massif: 
-                driver.find_element_by_id('datepicker').click()
-                print(f'no BRA available for this date: {date}')
-                continue
-            available_massif = [massif.text for massif in dropdown_massif.options]
-            for zone in available_massif:
-                time.sleep(1)
-                print(zone)
-                driver.find_element_by_xpath(f"//select[@id='select_massif']/option[text()='{zone}']").click()
-                driver.find_element_by_xpath("//select[@name='extension']/option[@value='xml']").click()
-                driver.find_element_by_xpath("//input[@value='Télécharger']").click()
-                link = driver.find_element_by_partial_link_text('Accès aux données')
-                url = link.get_attribute('href')
-                urlretrieve(url,
-                            f'bra_test/{zone}_{date}.xml')
-                driver.execute_script("window.history.go(-1)")
-                driver.find_element_by_xpath("//div[@class='publication-info telechargements replie']/h3[contains(., 'Téléchargement')]").click()
-                driver.find_element_by_id('datepicker').click()
-                driver.find_element_by_xpath(f"//select[@class='ui-datepicker-year']/option[text()='{year}']").click()
-                driver.find_element_by_xpath(f"//select[@class='ui-datepicker-month']/option[text()='{month}']").click()
-                
-    
-
-#########################################
-#########################################
-#########################################
-    
-
+       
+scrap_avalanche_report(driver, date_to_scrap, '../avalanche_forecasting/avalanche_report')
 
 
 ################################################
@@ -221,11 +147,11 @@ for bra_fp in glob.glob('bra_test/*'):
     tree = ET.parse(bra_fp)
     root = tree.getroot()
     info_xml = root[0].attrib
-    
+
     try:
         risque = root[0].findall('CARTOUCHERISQUE')[0]
     except Exception:
-        print(bra_fp)
+        print(f'NO CARTOUCHERISQUE AVAILABLE FOR {bra_fp}')
         continue
 
     cartoucherisque_element = ['PENTE',
@@ -234,7 +160,7 @@ for bra_fp in glob.glob('bra_test/*'):
                                'NATUREL',
                                'AVIS',
                                'VIGILANCE']
-    
+
     for elem in cartoucherisque_element:
         xml_elem = risque.findall(elem)[0].attrib
         if type(xml_elem) is dict:
@@ -242,20 +168,20 @@ for bra_fp in glob.glob('bra_test/*'):
                 info_xml[key] = val
         else:
             info_xml[elem] = xml_elem
-    
+
     text_elements = ['STABILITE',
                       'QUALITE']
-                      
+                  
     for elem in text_elements:
 
         info_xml[elem] = root[0].findall(elem)[0].findall('TEXTE')[0].text
-    
+
     for xml_elem in ['ENNEIGEMENT']:
         for elem, val in root[0].findall(xml_elem)[0].attrib.items():
             if elem == 'DATE':
                 elem = f'{elem}_MESURE_LIMITE_ENNEIGEMENT'
             info_xml[elem] = val
-        
+    
     for xml_elem in ['TENDANCES']:
         for child in root[0].findall(xml_elem)[0]:
             for elem, val in child.attrib.items():
@@ -275,108 +201,6 @@ for bra_fp in glob.glob('bra_test/*'):
 #########################################
 #########################################
 #########################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-url_bra = 'http://www.meteofrance.com/previsions-meteo-montagne/bulletin-avalanches'
-regions = [['chablais', 'opp01'], ['aravis', 'opp02'], ['mont-blanc', 'opp03'], ['bauges','opp04'], ['maurienne', 'opp09'],
-          ['vanoise', 'opp10'], ['beaufortain', 'opp05'], ['haute-tarentaise', 'opp06'], ['haute-maurienne', 'opp11'],
-          ['chartreuse', 'opp07'], ['vercors', 'opp14'], ['belledonne', 'opp08'], ['oisans', 'opp15'], ['grandes-rousses', 'opp12'],
-          ['thabor', 'opp13'], ['pelvoux', 'opp16'], ['champsaur', 'opp19'],
-          ['devoluy', 'opp18'], ['embrunais-parpallon','OPP20'],['queyras', 'opp17'], 
-          ['ubaye', 'opp21'], ['haut-var-haut-verdon', 'opp22'], ['mercantour', 'opp23']]
-
-for region in regions:
-    print(region[0])
-    day = datetime.datetime.today().strftime('%d_%m_%y')
-    url_pdf_bra = 'http://www.meteofrance.com/integration/sim-portail/generated/integration/img/produits/pdf/bulletins_bra'
-    urllib.request.urlretrieve('{}/{}.pdf'.format(url_pdf_bra, region[1].upper()),
-                               '../../divers/perso/avalanche_projet/bra/{}_{}.pdf'.format(region[0], day))
-
-
-
-
-
-
-
-from urllib.request import urlopen
-#url = "http://www.meteofrance.com/previsions-meteo-montagne/bulletin-avalanches/mont-blanc/OPP03"
-url = 'http://www.meteofrance.com/previsions-meteo-montagne/bulletin-avalanches/aravis/OPP02'
-html = urlopen(url)
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(html)
-
-images = soup.find_all('img')
-
-img = images[6].get('src')
-import base64
-head, data = img.split(',', 1)
-file_ext = head.split(';')[0].split('/')[1]
-plain_data = base64.b64decode(data)
-with open('image_bis.' + file_ext, 'wb') as f:
-    f.write(plain_data)
-
-
-for i, res in enumerate(soup.find_all('img')):
-    #print(res.get('src'))
-    print(i)
-    try:
-        list_var = url.split('/')
-        resource = urlopen(list_var[0]+"//"+list_var[2]+res.get('src'))
-        output = open('test/'+res.get('src').split('/')[-1],"wb")
-        output.write(resource.read())
-        output.close()
-    except:
-        print('FAILED')
-
-
-
-from scipy.misc import imread
-a = imread('image.png')
-b = imread('image_bis.png')
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots(1,2)
-ax[0].imshow(a[20:22, 225:230, :], cmap=plt.cm.gray)
-ax[1].imshow(b[3:108, 210:300, :], cmap=plt.cm.gray)
-
-
-from PIL import Image, ImageEnhance, ImageFilter
-import pytesseract
-text = pytesseract.image_to_string(Image.open('image_bis.png'))
-
-
-def download_pdf():
-    
-  url_bra = 'http://www.meteofrance.com/previsions-meteo-montagne/bulletin-avalanches'
-  regions = [['chablais', 'opp01'], ['aravis', 'opp02'], ['mont-blanc', 'opp03'], ['bauges','opp04'], ['maurienne', 'opp09'],
-            ['vanoise', 'opp10'], ['beaufortain', 'opp05'], ['haute-tarentaise', 'opp06'], ['haute-maurienne', 'opp11'],
-            ['chartreuse', 'opp07'], ['vercors', 'opp14'], ['belledonne', 'opp08'], ['oisans', 'opp15'], ['grandes-rousses', 'opp12'],
-            ['thabor', 'opp13'], ['pelvoux', 'opp16'], ['champsaur', 'opp19'],
-            ['devoluy', 'opp18'], ['embrunais-parpallon','OPP20'],['queyras', 'opp17'], 
-            ['ubaye', 'opp21'], ['haut-var-haut-verdon', 'opp22'], ['mercantour', 'opp23']]
-
-  for region in regions:
-      print(region[0])
-      day = datetime.datetime.today().strftime('%d_%m_%y')
-      url_pdf_bra = 'http://www.meteofrance.com/integration/sim-portail/generated/integration/img/produits/pdf/bulletins_bra'
-      urllib.request.urlretrieve('{}/{}.pdf'.format(url_pdf_bra, region[1].upper()),
-                                 'bra/{}_{}.pdf'.format(region[0], day))
-
-
 
 
 def download_data():
@@ -484,7 +308,6 @@ def put_type_float(data):
     return new_data
     
 
-
 def plot_correlation(data, feature=[]):
     
     if feature == [] :
@@ -508,14 +331,3 @@ def plot_correlation(data, feature=[]):
     plt.title('Correlation between different features')
     
     
-
-    
-    
-    
-
-
-
-
-
-
-
